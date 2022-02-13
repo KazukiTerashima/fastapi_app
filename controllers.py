c2e78b4582b1eaa834f82c9e46eb287130102b82
KazukiTerashima <terashima.kazuki@outlook.jp>
2022-02-13 17:02:27 +0900

    [issue #3]FastAPI動作のための準備 - Docker上での動作確認済み

---
+++ controllers.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from starlette.requests import Request
+
+app = FastAPI(
+    title="FastAPIでつくるtoDoアプリケーション",
+    description="FastAPIチュートリアル：FastAPI(とstarlette)でシンプルなtoDoアプリ",
+    version='0.9 beta'
+)
+
+
+def index(request: Request):
+    return {"Hello": "World"}
