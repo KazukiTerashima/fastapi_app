e7cdb41b5b3d1b7681055a26e7b5e772617ca44d
KazukiTerashima <terashima.kazuki@outlook.jp>
2022-02-13 23:40:31 +0900

    [issue #5] テンプレートエンジンの導入 - Jinja2の導入

--- controllers.py
+++ controllers.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from starlete.templating import Jinja2Templates
 from starlette.requests import Request
 
 app = FastAPI(
@@ -6,6 +7,10 @@ app = FastAPI(
     description="FastAPIチュートリアル：FastAPI(とstarlette)でシンプルなtoDoアプリ",
     version='0.9 beta'
 )
+
+# new テンプレート関連の設定(Jinja2)
+templates = Jinja2Templates(directory="templates")
+jinja_env = templates.env # Jinja2.Environment : filterやglobalの設定
 
 
 def index(request: Request):
